7cc9e338e3dfb09caa2d55d20d38caf4b1ccaaa3
leminhduy1912 <minhduyle081003@email.com>
2025-04-04 11:08:39 +0700

    done homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
-// import SpecialityMenu from '../components/SpecialityMenu'
-// import TopDoctors from '../components/TopDoctors'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      {/* <SpecialityMenu />
-      <TopDoctors /> */}
+      <SpecialityMenu />
+      <TopDoctors />
       <Banner />
     </div>
   )
